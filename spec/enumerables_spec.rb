c87320f5dbbeab1df8a83932c9f34c66293eecd0
[sam arsanios] <samson.kibrom@hotmail.com>
2021-01-21 12:33:08 +0300

    Remove commented code

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -173,15 +173,3 @@ describe Enumerable do
     end
   end
 end
-
-# puts '9.--------my_inject--------'
-# puts((1..5).my_inject { |sum, n| sum + n }) #=> 15
-# puts((1..5).my_inject(1) { |product, n| product * n }) #=> 120
-# puts((1..5).my_inject(1, :+)) #=> 16
-# longest =
-#   %w[cardiology anatomy neurology].my_inject do |memo, word|
-#     memo.length > word.length ? memo : word
-#   end
-# puts longest #=> "cardiology"
-
-# puts multiply_els([2, 4, 5]) #=> 40
